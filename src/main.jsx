9f27561fbe3f2d27a16a784171a8208db6e8aa39
Mrt-28 <marteau28210@hotmail.fr>
2025-09-04 13:20:02 +0200

    -added variable for css breakpoint -added min function on headband element for beter display -added Prettier on the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-  Navigate,
-} from "react-router-dom"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom'
 
 import './sass/bases/_reset.scss'
 import './sass/main.scss'
@@ -19,27 +14,27 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Outlet />
-        <Footer />
-      </>
-    ),
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/logement/:id", element: <Logement /> },
-      { path: "/404", element: <NotFound /> },
-      { path: "*", element: <Navigate to="/404" replace /> },
-    ],
-  },
+	{
+		path: '/',
+		element: (
+			<>
+				<Header />
+				<Outlet />
+				<Footer />
+			</>
+		),
+		children: [
+			{ path: '/', element: <Home /> },
+			{ path: '/about', element: <About /> },
+			{ path: '/logement/:id', element: <Logement /> },
+			{ path: '/404', element: <NotFound /> },
+			{ path: '*', element: <Navigate to="/404" replace /> },
+		],
+	},
 ])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
