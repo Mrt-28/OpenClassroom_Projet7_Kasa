15d0eac435f998e8c523512fbed26b454795cec2
Mrt-28 <marteau28210@hotmail.fr>
2025-09-05 16:44:42 +0200

    feat: Implement fade-in and fade-out animations for LogementCarousel component

--- src/components/LogementCarousel.jsx
+++ src/components/LogementCarousel.jsx
@@ -4,36 +4,82 @@ import arrowLeft from '../assets/arrow_left.svg'
 import arrowRight from '../assets/arrow_right.svg'
 
 function LogementCarousel(props) {
+	const [pictureIndex, setPictureIndex] = useState(0)
+	const pictures = props.pictures
 
-    const [pictureIndex, setPictureIndex] = useState(0)
-    const pictures = props.pictures
+	async function timeFadeOut(callback) {
+		return new Promise((resolve) => {
+			const picture = document.querySelector('.logement__carousel__picture-container__picture')
+			picture.classList.add('fade-out')
 
-    function left() {
-        if (pictureIndex > 0) {
-            setPictureIndex(pictureIndex - 1)
-        }
-        else {
-            setPictureIndex(pictures.length - 1)
-        }
-    }
-    function right() {
-        if (pictureIndex < pictures.length - 1) {
-            setPictureIndex(pictureIndex + 1)
-        }
-        else {
-            setPictureIndex(0)
-        }
-    }
+			setTimeout(() => {
+				callback()
+			}, 500)
 
-    return (
-        <div className="logement__carousel">
-            <img onClick={left} className="logement__carousel__left-arrow" src={arrowLeft} alt="Previous" />
-            <div className="logement__carousel__picture-container">
-                <img className="logement__carousel__picture-container__picture" src={pictures[pictureIndex]} alt={props.title} />
-            </div>
-            <img onClick={right} className="logement__carousel__right-arrow" src={arrowRight} alt="Next" />
-        </div>
-    )
+			setTimeout(() => {
+				picture.classList.remove('fade-out')
+				resolve()
+			}, 1000)
+		})
+	}
+
+	function timeFadein() {
+		var picture = document.querySelector('.logement__carousel__picture-container__picture')
+		picture.classList.add('fade-in')
+		setTimeout(() => {
+			picture.classList.remove('fade-in')
+		}, 1000)
+	}
+
+	async function left() {
+		const changeIndex = () => {
+			if (pictureIndex > 0) {
+				setPictureIndex(pictureIndex - 1)
+			} else {
+				setPictureIndex(pictures.length - 1)
+			}
+		}
+
+		await timeFadeOut(changeIndex)
+		timeFadein()
+	}
+
+	async function right() {
+		const changeIndex = () => {
+			if (pictureIndex < pictures.length - 1) {
+				setPictureIndex(pictureIndex + 1)
+			} else {
+				setPictureIndex(0)
+			}
+		}
+
+		await timeFadeOut(changeIndex)
+		timeFadein()
+	}
+
+	return (
+		<div className="logement__carousel">
+			<img
+				onClick={left}
+				className="logement__carousel__left-arrow"
+				src={arrowLeft}
+				alt="Previous"
+			/>
+			<div className="logement__carousel__picture-container">
+				<img
+					className="logement__carousel__picture-container__picture"
+					src={pictures[pictureIndex]}
+					alt={props.title}
+				/>
+			</div>
+			<img
+				onClick={right}
+				className="logement__carousel__right-arrow"
+				src={arrowRight}
+				alt="Next"
+			/>
+		</div>
+	)
 }
 
 export default LogementCarousel
